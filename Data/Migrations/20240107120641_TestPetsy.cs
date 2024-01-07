f532f0cb29b5f6fd192eb9d3305d0f41278be164
Marija Cholakova <marijach@iborn.net>
2024-01-07 16:43:49 +0100

    Add project files.

---
+++ Data/Migrations/20240107120641_TestPetsy.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Petsy.Models.Migrations
+{
+    public partial class TestPetsy : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pets_People_PersonId",
+                table: "Pets");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PersonId",
+                table: "Pets",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pets_People_PersonId",
+                table: "Pets",
+                column: "PersonId",
+                principalTable: "People",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pets_People_PersonId",
+                table: "Pets");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PersonId",
+                table: "Pets",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pets_People_PersonId",
+                table: "Pets",
+                column: "PersonId",
+                principalTable: "People",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Age { get; set; }
+    public List<Pet> Pets { get; set; }
+
+    public Person()
+    {
+        Pets = new List<Pet>();
+    }
+
+    [DisplayName("Owner's Full Name")]
+	public string GetFullName
+	{
+		get { return String.Format("{0} {1}", Name, Surname); }
+	}
+
+}
